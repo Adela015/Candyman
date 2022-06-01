4c9af90cf5b1cc21bbb76051ad5835ef53bef963
Adela015 <ade.cervantes01@gmail.com>
2022-06-01 20:43:36 -0300

    MVC implementado

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Rutas
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/index.html'))
+});
+
+
+
+
+// Levantamos el servidor con app.listen, recibe dos parámetros, 
+// el PUERTO y opcionalemente un callback que se ejecutará si el seridor se inicia
+app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000 <3"));
+
